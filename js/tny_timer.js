f668c1e634e673c699cc8220b979b638c03afdd5
MJSantos1s <MJSantos1s@capetigers.com>
2026-01-23 10:11:46 -0600

    opopop

--- js/tny_timer.js
+++ js/tny_timer.js
@@ -9,32 +9,6 @@
    Date:   
 
 */
-showClock();
-setInterval("runClock()", 1000)
-function showClock() {
-   var thisDay = Date();
-   var localDate = thisday.toLocaleDateString();
-   var localTime = thisDay.toLocaleTimeString();
-   var j4Date = nextJuly4(thisDay);
-   j4Date = setHours(21);
-   var minsLeft = (hrsLeft - Math.floor(hrsLeft))*60;
-   var secsLeft = (minsLeft - Math.floor(minsLeft))*60;
-   document.getElementById("days").textContent = Math.floor(daysLeft);
-   document.getElementById("hrs").textContent = Math.floor(hrsLeft);
-   document.getElementById("mins").textContent = Math.floor(minsLeft);
-   document.getElementById("secs").textContent = Math.floor(secsLeft);
-   document.getElementById("dateNow").innerHTML =
-   localDate + "<br />" + localTime;
-}
-
-function nextJuly4(currentDate) {
-   var cYear = currentDate.getFullYear();
-   var jDate = new Date("July 4, 2088");
-   jDate.setFullYear(cYear);
-   if ((jDate - currentDate) < 0) jDate.setFullYear(cYear + 1);
-   return jDate;
-}
-
 function getTimeRemaining(endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
   var seconds = Math.floor((t / 1000) % 60);
